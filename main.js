0c628de55703a36347cd0a30c6037698d06b081a
dimplembagkar <dimplembagkar>
2022-10-27 12:34:25 +0530

    my aim

---
+++ main.js
@@ -0,0 +1,24 @@
+function code()
+{
+    window.location="index1.html";
+}
+
+function doc()
+{
+    window.location="index2.html";
+}
+
+function officer()
+{
+    window.location="index3.html";
+}
+
+function teach()
+{
+    window.location="index4.html";
+}
+
+function goBack()
+{
+    window.location="index.html";
+}
